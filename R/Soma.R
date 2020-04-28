8ca3b72d5eaeed6f02afbbf6c847199da79b34f3
lurbento <lurbento@voegol.com.br>
2020-04-27 21:46:20 -0300

    Initial commit

---
+++ R/Soma.R
@@ -0,0 +1,24 @@
+#' Soma dois nemeros
+#'
+#'  Recebe um numero x e outro y e retorna a soma de x e y.
+#'
+#' @param x primeiro argumento
+#' @param y segundo argumento
+#'
+#' @return a soma de dois numeros
+#'
+#' @export
+
+soma <- function(x, y){
+  x + y
+}
+
+soma(1,2)
+
+#Funções
+#usethis::create_package()
+#usethis::use_mit_license()
+#usethis::use_readme_md()
+#usethis::use_data(mtcars)
+#usethis::use_dataraw
+

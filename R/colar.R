595290effd71a9b76b8ac575393861d23582f0df
lurbento <lurbento@voegol.com.br>
2020-04-27 21:57:32 -0300

    Initial commit

---
+++ R/colar.R
@@ -0,0 +1,16 @@
+#' Soma dois nemeros
+#'
+#'  Recebe um numero x e outro y e retorna a soma de x e y.
+#'
+#' @param x primeiro argumento
+#' @param y segundo argumento
+#'
+#' @return a soma de dois numeros
+#'
+#' @export
+
+colar <- function(x, y){
+  paste(x,y)
+}
+
+colar(1,2)
